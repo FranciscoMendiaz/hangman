7e13228b1070d908c7e5ba572a1b3bd440fd3b7e
fabriortenzi <fabriortenzi10@gmail.com>
2024-11-18 19:05:03 -0300

    config prettier and eslint

---
+++ src/services/game.ts
@@ -0,0 +1,46 @@
+import { Hangman } from './hangman';
+
+export const maxLives = 5;
+
+export class Game {
+  hangman: Hangman;
+  lives: number = maxLives;
+
+  // To see if player won
+  correctGuesses: number = 0;
+
+  guesses: { letter: string; belongsToWord: boolean }[] = [];
+
+  constructor(correctWord: string) {
+    this.hangman = new Hangman(correctWord);
+  }
+
+  takeGuess(letter: string) {
+    try {
+      let accerted = this.hangman.checkLetter(letter);
+      this.correctGuesses = this.correctGuesses + accerted.length;
+      this.addCorrectGuess(letter);
+      if (this.correctGuesses === this.hangman.word.length) return 'Win';
+      return accerted;
+    } catch (error) {
+      if (error !== 'Only one letter is allowed') {
+        this.addIncorrectGuess(letter);
+        this.subtractLife();
+        if (this.lives === 0) throw 'Game Over';
+      }
+      throw error;
+    }
+  }
+
+  addIncorrectGuess(letter: string) {
+    this.guesses.push({ letter: letter, belongsToWord: false });
+  }
+
+  addCorrectGuess(letter: string) {
+    this.guesses.push({ letter: letter, belongsToWord: true });
+  }
+
+  subtractLife() {
+    this.lives -= 1;
+  }
+}

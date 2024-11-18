97ed71ce20f7b6985febd23f21db3a9f4c8aec34
fabriortenzi <fabriortenzi10@gmail.com>
2024-11-18 18:02:34 -0300

    correct some tests and delete Guess class

--- src/game.ts
+++ src/game.ts
@@ -5,7 +5,10 @@ export const maxLives = 5
 export class Game {
   hangman: Hangman
   lives: number = maxLives
+
+  // To see if player won
   correctGuesses: number = 0
+
   guesses: { letter: string, belongsToWord: boolean }[] = []
 
   constructor(correctWord: string) {
@@ -18,12 +21,12 @@ export class Game {
       this.correctGuesses = this.correctGuesses + accerted.length
       this.addCorrectGuess(letter)
       if (this.correctGuesses === this.hangman.word.length) return ("Win")
-      return ("The letter belongs to the word")
+      return (accerted)
     } catch (error) {
       if (error !== "Only one letter is allowed") {
         this.addIncorrectGuess(letter)
         this.subtractLife()
-        if (this.lives == 0) throw "Game Over"
+        if (this.lives === 0) throw "Game Over"
       }
       throw error
     }
